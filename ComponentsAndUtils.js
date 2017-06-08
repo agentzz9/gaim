e20daff39a01c90560a9cb64e808948bd07494bb
Sparsh Priyadarshi <sparsh11194@gmail.com>
2017-06-09 03:29:22 +0530

    added faulty text start click, modified start page game loop

--- ComponentsAndUtils.js
+++ ComponentsAndUtils.js
@@ -6,10 +6,8 @@ function Component(width, height, color, x, y, type) {
     	//this.image.src = color;//take from profile? camera ?
     	//above code snippet giving issues apparently the image has to be already loaded:(
   		this.image = document.getElementById(color);
-
-  	}
-
-	this.width = width;
+    }
+    this.width = width;
 	this.height = height;
 	this.color = color;
 	this.x = x;
@@ -17,7 +15,6 @@ function Component(width, height, color, x, y, type) {
 	
     this.speedX = 0;
 	this.speedY = 0;
-
 
 	this.update = function() {
         ctx = gameArea.context;
@@ -54,20 +51,35 @@ function Component(width, height, color, x, y, type) {
         }
         return crash;
     }
+    this.isClicked = function() {
 
-    this.clicked = function() {
-        var myleft = this.x;
-        var myright = this.x + (this.width);
-        var mytop = this.y;
-        var mybottom = this.y + (this.height);
-        var clicked = true;
-        if ((mybottom < gameArea.y) || (mytop > gameArea.y)
-         || (myright < gameArea.x) || (myleft > gameArea.x)) {
-            clicked = false;
+        if(type == "text"){
+            var tempWidth = gameArea.context.measureText(this.text).width;
+            var tempHeight = gameArea.context.measureText(this.text).height;//doesnt work. hheight alternate approach needed 
+            var myleft = this.x;
+            var myright = this.x + (tempWidth);
+            var mytop = this.y;
+            var mybottom = this.y + (tempHeight);
+            var wasIclicked = true;
+            if ((mybottom < gameArea.y) || (mytop > gameArea.y)
+             || (myright < gameArea.x) || (myleft > gameArea.x)) {
+                wasIclicked = false;
+            }
+            return wasIclicked;
+
+        }else{
+            var myleft = this.x;
+            var myright = this.x + (this.width);
+            var mytop = this.y;
+            var mybottom = this.y + (this.height);
+            var wasIclicked = true;
+            if ((mybottom < gameArea.y) || (mytop > gameArea.y)
+             || (myright < gameArea.x) || (myleft > gameArea.x)) {
+                wasIclicked = false;
+            }
+            return wasIclicked;
         }
-        return clicked;
     }
-
 }
 function RoadlineComponent(width, height, color, x, y, type) {
 
@@ -77,7 +89,6 @@ function RoadlineComponent(width, height, color, x, y, type) {
     	//this.image.src = color;//take from profile? camera ?
     	//above code snippet giving issues apparently the image has to be already loaded:(
   		this.image = document.getElementById(color);
-
   	}
 
 	this.width = width;
@@ -88,7 +99,6 @@ function RoadlineComponent(width, height, color, x, y, type) {
 	
     this.speedX = 0;
 	this.speedY = 0;
-
 
 	this.update = function() {
         ctx = gameArea.context;
@@ -146,77 +156,6 @@ function RoadlineComponent(width, height, color, x, y, type) {
     }
 
 }
-function Component(width, height, color, x, y, type) {
-
-	this.type = type;
-	if (type == "image") {
-    	//this.image = new Image();
-    	//this.image.src = color;//take from profile? camera ?
-    	//above code snippet giving issues apparently the image has to be already loaded:(
-  		this.image = document.getElementById(color);
-
-  	}
-
-	this.width = width;
-	this.height = height;
-	this.color = color;
-	this.x = x;
-	this.y = y;
-	
-    this.speedX = 0;
-	this.speedY = 0;
-
-
-	this.update = function() {
-        ctx = gameArea.context;
-        if (type == "image") {
-      		ctx.drawImage(this.image, 
-        	this.x, 
-        	this.y,
-        	this.width, this.height);
-   		}else if (this.type == "text") {
-            ctx.font = this.width + " " + this.height;
-            ctx.fillStyle = color;
-            ctx.fillText(this.text, this.x, this.y);
-        } else { //rect
-            ctx.fillStyle = color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-        }
-    }
-	this.newPos = function() {
-        this.x += this.speedX;
-        this.y += this.speedY;        
-    }
-	this.crashWith = function(otherobj) {
-        var myleft = this.x;
-        var myright = this.x + (this.width);
-        var mytop = this.y;
-        var mybottom = this.y + (this.height);
-        var otherleft = otherobj.x;
-        var otherright = otherobj.x + (otherobj.width);
-        var othertop = otherobj.y;
-        var otherbottom = otherobj.y + (otherobj.height);
-        var crash = true;
-        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
-            crash = false;
-        }
-        return crash;
-    }
-
-    this.isTouched = function() {
-        var myleft = this.x;
-        var myright = this.x + (this.width);
-        var mytop = this.y;
-        var mybottom = this.y + (this.height);
-        var touched = true;
-        if ((mybottom < myGameArea.y) || (mytop > myGameArea.y) //TODO for multi touch checks refactor this.
-         || (myright < myGameArea.x) || (myleft > myGameArea.x)) {
-            touched = false;
-        }
-        return touched;
-    }
-
-}
 
 function PlayerCarComponent(width, height, color, x, y, type) {
 
@@ -226,7 +165,6 @@ function PlayerCarComponent(width, height, color, x, y, type) {
         //this.image.src = color;//take from profile? camera ?
         //above code snippet giving issues apparently the image has to be already loaded:(
         this.image = document.getElementById(color);
-
     }
 
     this.width = width;
@@ -238,7 +176,6 @@ function PlayerCarComponent(width, height, color, x, y, type) {
     
     this.speedX = 0;
     this.speedY = 0;
-
 
     this.update = function() {
         ctx = gameArea.context;
@@ -379,4 +316,3 @@ function OtherCarComponent(width, height, color, x, y, type) {
     }
 
 }
-
